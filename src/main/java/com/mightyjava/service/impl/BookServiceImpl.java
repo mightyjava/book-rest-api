9a6fdc68c2f5090b1d90cbf4f165866ebc25c002
mightyjava <jeetparmar949@gmail.com>
2019-06-21 21:33:41 +0530

    Added CommandLineRunner

--- src/main/java/com/mightyjava/service/impl/BookServiceImpl.java
+++ src/main/java/com/mightyjava/service/impl/BookServiceImpl.java
@@ -17,22 +17,6 @@ public class BookServiceImpl implements IService<Book> {
 	@Autowired
 	private BookRepository bookRepository;
 
-//	private Long bookId = 100L;
-//	private Map<Long, Book> bookMap = new HashMap<Long, Book>();
-//
-//	{
-//		Book book = new Book();
-//		book.setId(bookId);
-//		book.setTitle("Spring Microservices in Action");
-//		book.setAuthor("John Carnell");
-//		book.setCoverPhotoURL(
-//				"https://images-na.ssl-images-amazon.com/images/I/417zLTa1uqL._SX397_BO1,204,203,200_.jpg");
-//		book.setIsbnNumber(1617293989L);
-//		book.setPrice(2776.00);
-//		book.setLanguage("English");
-//		bookMap.put(book.getId(), book);
-//	}
-
 	@Override
 	public Collection<Book> findAll() {
 		return bookRepository.findAll();
